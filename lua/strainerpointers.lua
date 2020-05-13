eee328226b3a61bf218af2991b2e177c2a3bfb31
aapplesauc <65298840+aapplesauc@users.noreply.github.com>
2020-05-13 10:23:43 -0500

    Add files via upload

---
+++ lua/strainerpointers.lua
@@ -0,0 +1,23 @@
+--Memory Pointers (for version 4.1)
+varGoto = "ShovelKnight.exe+934088"
+varCharacter = "ShovelKnight.exe+8dcf50"
+--varDialogue = "ShovelKnight.exe+792DFC"
+--varOverworld = "ShovelKnight.exe+7FC6D8"
+varStage = "ShovelKnight.exe+934084"
+--varPosXalt = "[[[[[ShovelKnight.exe+7a6cdc]+4]+1c]+44]+60]+c"
+--varPosYalt = "[[[[[ShovelKnight.exe+7a6cdc]+4]+1c]+44]+60]+10"
+varPosX = "[[[ShovelKnight.exe+8D2318]+30]+24]+c"
+varPosY = "[[[ShovelKnight.exe+8D2318]+30]+24]+10"
+varCheckX = "ShovelKnight.exe+8F123C"
+varCheckY = "ShovelKnight.exe+8F1240"
+varCheckFlag = "ShovelKnight.exe+8F124c"
+varCheckRespawn = "ShovelKnight.exe+8F1258"
+varIntro = "ShovelKnight.exe+8F22a8"
+varHealth = "[ShovelKnight.exe+8D2318]+e8"
+varMoney = "[ShovelKnight.exe+8D2318]+3dc"
+--varCameraLock = "[[[[[ShovelKnight.exe+006FFBA0]+7fc]+7e0]+438]+438]+3e0"
+varBossHealth = "[[[[[[[[ShovelKnight.exe+8D1EF0]+14]+98]+0]+140]+8]+104]+184]+20"
+varMana = "[ShovelKnight.exe+8D2318]+3d8"
+
+--commented pointers are outdated, but aren't used
+--I'd use varCameraLock but I don't know where to find it
